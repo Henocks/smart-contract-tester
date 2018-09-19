7e9f239748bdaa03bac4e2e7fab059b65ccad173
Henocks <hr4574@naver.com>
2018-09-19 16:11:51 +0900

    new function call structure

--- src/components/contractLoader.js
+++ src/components/contractLoader.js
@@ -10,13 +10,21 @@ class contractLoader extends React.Component {
     };
   }
 
-  componentDidMount() {
+  testRunner(testList, contract) {
+    let testResult = [];
+    testList.forEach(async (test) => {
+      //testResult.push(await Promise.resolve(contract.methods.balanceOf(test.param[0]).call()));
+      eval("contract.methods." + test.func + "(test.param[0]).call().then((result) => {testResult.push(result)})");
+    });
+    console.log(testResult);
+    return Promise.resolve(testResult);
+  }
 
+  render() {
     const ABI = [{ "constant": true, "inputs": [], "name": "name", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "approve", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "vaultID", "type": "uint256" }], "name": "terminateGRC", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "vaultList", "outputs": [{ "name": "master", "type": "address" }, { "name": "value", "type": "uint256" }, { "name": "total", "type": "uint256" }, { "name": "timestamp", "type": "uint256" }, { "name": "status", "type": "uint8" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [{ "name": "", "type": "uint8" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_value", "type": "uint256" }], "name": "burn", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "_value", "type": "uint256" }], "name": "depositGRC", "outputs": [{ "name": "success", "type": "bool" }, { "name": "vID", "type": "uint256" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }], "name": "balanceOf", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_from", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "burnFrom", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transfer", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "vaultID", "type": "uint256" }], "name": "stakeGRC", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }, { "name": "_extraData", "type": "bytes" }], "name": "approveAndCall", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }, { "name": "", "type": "address" }], "name": "allowance", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [{ "name": "initialSupply", "type": "uint256" }, { "name": "tokenName", "type": "string" }, { "name": "tokenSymbol", "type": "string" }], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "from", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Burn", "type": "event" }];
     const Addr = "0x1179cf51c590794b0897586bc83a8d82ef8a243b";
     const web3 = new Web3(Web3.givenProvider);
     const contract = new web3.eth.Contract(ABI, Addr);
-    
     const testList = [
       {
         func : "balanceOf",
@@ -27,21 +35,19 @@ class contractLoader extends React.Component {
         param : ["0x9cad9b4941aafb67b5a5e6dea657db2d4ea7b757"]
       },
       {
-        func : "GSCCount",
-        param : []
-      }
+        func : "balanceOf",
+        param : ["0x9cad9b4941aafb67b5a5e6dea657db2d4ea7b757"]
+      },
     ];
 
-    console.log(testList);
-
-    for(let i = 0; i < 3; i++) {
-      eval("contract.methods." + testList[i].func + "(testList[i].param[0]).call().then((_result) => {this.setState({result : [...this.state.result, _result]})});");
-      console.log(this.state.result);
-    }
+    this.testRunner(testList, contract).then((_result) => {
+      this.setState({
+        result: _result
+      });
+    });
     
-  }
+    //const result = this.testRunner(testList, contract);
 
-  render() {
     return (
       <div>
         <h2>Func1 Result : {this.state.result[0]}</h2>
