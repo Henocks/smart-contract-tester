43596b616d1a2f4445942a3777b5a27037147046
Henocks <hr4574@naver.com>
2018-09-21 14:32:46 +0900

    Fixed loader options
    ABI misplaced. fixed it.

--- src/components/contractLoader.js
+++ src/components/contractLoader.js
@@ -12,34 +12,37 @@ class contractLoader extends React.Component {
 
   //async reader()
   componentWillMount() {
-    const ABI = [{ "constant": true, "inputs": [], "name": "name", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "approve", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "vaultID", "type": "uint256" }], "name": "terminateGRC", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "totalSupply", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_from", "type": "address" }, { "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "vaultList", "outputs": [{ "name": "master", "type": "address" }, { "name": "value", "type": "uint256" }, { "name": "total", "type": "uint256" }, { "name": "timestamp", "type": "uint256" }, { "name": "status", "type": "uint8" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "decimals", "outputs": [{ "name": "", "type": "uint8" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_value", "type": "uint256" }], "name": "burn", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "_value", "type": "uint256" }], "name": "depositGRC", "outputs": [{ "name": "success", "type": "bool" }, { "name": "vID", "type": "uint256" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }], "name": "balanceOf", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_from", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "burnFrom", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "symbol", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "_to", "type": "address" }, { "name": "_value", "type": "uint256" }], "name": "transfer", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "vaultID", "type": "uint256" }], "name": "stakeGRC", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [{ "name": "_spender", "type": "address" }, { "name": "_value", "type": "uint256" }, { "name": "_extraData", "type": "bytes" }], "name": "approveAndCall", "outputs": [{ "name": "success", "type": "bool" }], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "address" }, { "name": "", "type": "address" }], "name": "allowance", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [{ "name": "initialSupply", "type": "uint256" }, { "name": "tokenName", "type": "string" }, { "name": "tokenSymbol", "type": "string" }], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "from", "type": "address" }, { "indexed": true, "name": "to", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "name": "from", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }], "name": "Burn", "type": "event" }];
-    const Addr = "0x1179cf51c590794b0897586bc83a8d82ef8a243b";
+    const ABI = [{ "constant": true, "inputs": [], "name": "GSCCount", "outputs": [{ "name": "", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCHistory", "outputs": [{ "name": "history", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "GSCList", "outputs": [{ "name": "mainWallet", "type": "address" }, { "name": "value", "type": "uint256" }, { "name": "deposit", "type": "uint256" }, { "name": "Gchain_all", "type": "uint256" }, { "name": "status", "type": "uint256" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "verify", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCRatio", "outputs": [{ "name": "ratio", "type": "uint256[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "pay", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCAddr", "outputs": [{ "name": "Gchain", "type": "address[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "Gchain", "type": "address[]" }, { "name": "value", "type": "uint256" }, { "name": "ratio", "type": "uint256[]" }], "name": "createGSC", "outputs": [{ "name": "GSCID", "type": "uint256" }], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": false, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "abort", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [{ "name": "GSCID", "type": "uint256" }], "name": "getGSCTimestamp", "outputs": [{ "name": "timestamp", "type": "uint256[]" }], "payable": false, "stateMutability": "view", "type": "function" }, { "anonymous": false, "inputs": [], "name": "aborted", "type": "event" }, { "anonymous": false, "inputs": [], "name": "purchaseConfirmed", "type": "event" }, { "anonymous": false, "inputs": [], "name": "paymentDone", "type": "event" }];
+    const Addr = "0xE8720CB8b80ffb4D93BeE736C624dc547603fc49";
     const web3 = new Web3(Web3.givenProvider);
     const contract = new web3.eth.Contract(ABI, Addr);
     const testList = [
       {
-        func : "balanceOf",
-        param : ["0x8cad9b4941aafb67b5a5e6dea657db2d4ea7b757"]
+        func : "getGSCRatio",
+        param : [1]
       },
       {
-        func : "balanceOf",
-        param : ["0x9cad9b4941aafb67b5a5e6dea657db2d4ea7b757"]
+        func : "getGSCTimestamp",
+        param : [1]
       },
       {
-        func : "balanceOf",
-        param : ["0x9cad9b4941aafb67b5a5e6dea657db2d4ea7b757"]
+        func : "getGSCAddr",
+        param : [1]
+      },
+      {
+        func : "getGSCRatio",
+        param : [8]
       },
     ];
-
     let testResult = [];
 
-    console.log(testList);
+    const resultUpdater = (result) => {
+      testResult.push(result);
+      this.setState({result : testResult});
+    }
 
     testList.forEach(async (test) => {
-      //testResult.push(await Promise.resolve(contract.methods.balanceOf(test.param[0]).call()));
-      eval("contract.methods." + test.func + "(test.param[0]).call().then((result) => {testResult.push(result); this.setState({result : testResult});})");
-      
-      console.log(this.state);
+      eval("contract.methods." + test.func + "(test.param[0]).call().then((result) => {resultUpdater(result)})");
     });
   }
 
@@ -49,6 +52,7 @@ class contractLoader extends React.Component {
         <h2>Func1 Result : {this.state.result[0]}</h2>
         <h2>Func2 Result : {this.state.result[1]}</h2>
         <h2>Func3 Result : {this.state.result[2]}</h2>
+        <h2>Func4 Result : {this.state.result[3]}</h2>
       </div>
     );
   }
